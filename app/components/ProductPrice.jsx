27680b66df0d08cb86ed4458c7767951e0260235
Rowena Foo <rwnfoo@gmail.com>
2024-10-07 16:17:37 -0400

    pdp

--- app/components/ProductPrice.jsx
+++ app/components/ProductPrice.jsx
@@ -1,28 +1,110 @@
-import {Money} from '@shopify/hydrogen';
+import {useLoaderData} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
+import ProductGrid from '../components/ProductGrid';
+import {getPaginationVariables} from '@shopify/hydrogen';
 
-/**
- * @param {{
- *   price?: MoneyV2;
- *   compareAtPrice?: MoneyV2 | null;
- * }}
- */
-export function ProductPrice({price, compareAtPrice}) {
+export async function loader({params, context, request}) {
+  const paginationVariables = getPaginationVariables(request, {
+    pageBy: 4,
+  });
+  const {handle} = params;
+  const {collection} = await context.storefront.query(COLLECTION_QUERY, {
+    variables: {
+      ...paginationVariables,
+      handle,
+    },
+  });
+
+  // Handle 404s
+  if (!collection) {
+    throw new Response(null, {status: 404});
+  }
+
+  // json is a Remix utility for creating application/json responses
+  // https://remix.run/docs/en/v1/utils/json
+  return json({
+    collection,
+  });
+}
+
+export default function Collection() {
+  const {collection} = useLoaderData();
   return (
-    <div className="product-price">
-      {compareAtPrice ? (
-        <div className="product-price-on-sale">
-          {price ? <Money data={price} /> : null}
-          <s>
-            <Money data={compareAtPrice} />
-          </s>
-        </div>
-      ) : price ? (
-        <Money data={price} />
-      ) : (
-        <span>&nbsp;</span>
-      )}
-    </div>
+    <>
+      <header className="grid w-full gap-8 py-8 justify-items-start">
+        <h1 className="text-4xl whitespace-pre-wrap font-bold inline-block">
+          {collection.title}
+        </h1>
+
+        {collection.description && (
+          <div className="flex items-baseline justify-between w-full">
+            <div>
+              <p className="max-w-md whitespace-pre-wrap inherit text-copy inline-block">
+                {collection.description}
+              </p>
+            </div>
+          </div>
+        )}
+      </header>
+      <ProductGrid
+        collection={collection}
+        url={`/collections/${collection.handle}`}
+      />
+    </>
   );
 }
 
-/** @typedef {import('@shopify/hydrogen/storefront-api-types').MoneyV2} MoneyV2 */
+const COLLECTION_QUERY = `#graphql
+  query CollectionDetails(
+    $handle: String!
+    $first: Int
+    $last: Int
+    $startCursor: String
+    $endCursor: String
+  ) {
+    collection(handle: $handle) {
+      id
+      title
+      description
+      handle
+      products(
+        first: $first,
+        last: $last,
+        before: $startCursor,
+        after: $endCursor,
+      ) {
+        nodes {
+          id
+          title
+          publishedAt
+          handle
+          variants(first: 1) {
+            nodes {
+              id
+              image {
+                url
+                altText
+                width
+                height
+              }
+              price {
+                amount
+                currencyCode
+              }
+              compareAtPrice {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
+        pageInfo {
+          hasPreviousPage
+          hasNextPage
+          startCursor
+          endCursor
+        }
+      }
+    }
+  }
+`
